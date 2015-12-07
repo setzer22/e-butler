0c7dfdd22ac9b28deddbfd7c9ed54c1e0cc5c5a4
Luis Delicado <l.delicado33@gmail.com>
2015-12-07 11:38:32 +0100

    Añadida parte a configurador

--- src/main/java/org/ebutler/configurador_bot/ManagerConfiguracion.java
+++ src/main/java/org/ebutler/configurador_bot/ManagerConfiguracion.java
@@ -1,18 +1,71 @@
 package org.ebutler.configurador_bot;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.event.ActionEvent;
 
 @ManagedBean(name = "configuracion")
 public class ManagerConfiguracion {
 	
 	private boolean apagarOrdenador = true;
+	private String URL;
+	private String token;
 
+	private List<String> numerosAutorizados;
+	private String numeroNuevo = "";
+	
+	public void addNumber(ActionEvent actionEvent) {
+		numerosAutorizados.add(numeroNuevo);
+		numeroNuevo = "";
+	}
+	@PostConstruct
+	public void init() {
+		numerosAutorizados = new ArrayList<String>();
+		numerosAutorizados.add("hola");
+		System.out.println(numerosAutorizados);
+	}
+	
 	public boolean isApagarOrdenador() {
 		return apagarOrdenador;
 	}
 
 	public void setApagarOrdenador(boolean apagarOrdenador) {
 		this.apagarOrdenador = apagarOrdenador;
+	}
+
+	public String getURL() {
+		return URL;
+	}
+
+	public void setURL(String uRL) {
+		URL = uRL;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public List<String> getNumerosAutorizados() {
+		return numerosAutorizados;
+	}
+
+	public void setNumerosAutorizados(List<String> numerosAutorizados) {
+		this.numerosAutorizados = numerosAutorizados;
+	}
+
+	public String getNumeroNuevo() {
+		return numeroNuevo;
+	}
+
+	public void setNumeroNuevo(String numeroNuevo) {
+		this.numeroNuevo = numeroNuevo;
 	}
 
 }
