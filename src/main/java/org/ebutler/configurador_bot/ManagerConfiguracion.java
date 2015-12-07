12950c9b315a3222fb8f484177ed5783219edcff
Luis Delicado <l.delicado33@gmail.com>
2015-12-07 23:34:05 +0100

    Añadidos elementos de la pantalla de configuración y empezar a que sean funcionales

--- src/main/java/org/ebutler/configurador_bot/ManagerConfiguracion.java
+++ src/main/java/org/ebutler/configurador_bot/ManagerConfiguracion.java
@@ -1,29 +1,49 @@
 package org.ebutler.configurador_bot;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.event.ActionEvent;
+import javax.faces.bean.SessionScoped;
+
+import org.primefaces.model.UploadedFile;
 
 @ManagedBean(name = "configuracion")
-public class ManagerConfiguracion {
+@SessionScoped
+public class ManagerConfiguracion implements Serializable{
 	
 	private boolean apagarOrdenador = true;
+	
 	private String URL;
 	private String token;
-
+	private String mensajeNoAuto;
+	
 	private List<String> numerosAutorizados;
-	private String numeroNuevo = "";
+	private String numeroNuevo;
+	
+	private String pathScripts;
+	private UploadedFile file;
+	
+	private String pathAccesible;
+	private String pathTeclado;
+	
+	public void save() {
+		
+	}
 	
 	public void addNumber() {
 		numerosAutorizados.add(numeroNuevo);
 		numeroNuevo = "";
 	}
 	
+	public void deleteNumber() {
+		numerosAutorizados.remove(numeroNuevo);
+		numeroNuevo = "";
+	}
+	
 	public ManagerConfiguracion() {
 		numerosAutorizados = new ArrayList<String>();
-		numerosAutorizados.add("hola");
 	}
 	
 	public boolean isApagarOrdenador() {
@@ -35,7 +55,8 @@ public class ManagerConfiguracion {
 	}
 
 	public String getURL() {
-		return URL;
+		System.out.println(URL);
+		return URL;		
 	}
 
 	public void setURL(String uRL) {
@@ -64,6 +85,46 @@ public class ManagerConfiguracion {
 
 	public void setNumeroNuevo(String numeroNuevo) {
 		this.numeroNuevo = numeroNuevo;
+	}
+
+	public String getMensajeNoAuto() {
+		return mensajeNoAuto;
+	}
+
+	public void setMensajeNoAuto(String mensajeNoAuto) {
+		this.mensajeNoAuto = mensajeNoAuto;
+	}
+
+	public String getPathScripts() {
+		return pathScripts;
+	}
+
+	public void setPathScripts(String pathScripts) {
+		this.pathScripts = pathScripts;
+	}
+
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+
+	public String getPathAccesible() {
+		return pathAccesible;
+	}
+
+	public void setPathAccesible(String pathAccesible) {
+		this.pathAccesible = pathAccesible;
+	}
+
+	public String getPathTeclado() {
+		return pathTeclado;
+	}
+
+	public void setPathTeclado(String pathTeclado) {
+		this.pathTeclado = pathTeclado;
 	}
 
 }
